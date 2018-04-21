a1c8c49c186047f57cec40bca3ea9d50eb0798f5
PramitMallick <pramitmallick@gmail.com>
2018-04-21 13:36:42 -0400

    try

--- FaceRecognition/faceRecog.py
+++ FaceRecognition/faceRecog.py
@@ -110,6 +110,7 @@ def main():
     modelPath = '../SFD_pytorch/s3fd_convert.pth'
 
     use_cuda = torch.cuda.is_available()
+    use_cuda = False
     print("Using GPU - ",use_cuda)
 
     net = loadNet(numberOfClasses, modelPath)
